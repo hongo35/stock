8824d9b91d7ace163c6010e3cda404c251b62655
Yutaka Hongo <hongo@userlocal.jp>
2014-11-30 03:15:11 +0900

    [fix]株価データクロールスクリプト修正

--- price.py
+++ price.py
@@ -17,19 +17,24 @@ def main():
 
 	q = jsm.Quotes()
 
-	start_date = datetime.date(2014,11,1)
-	end_date   = datetime.date.today()
+	start_date = datetime.date(2014,10,1)
+	end_date   = datetime.date(2014,11,4)
 
-	cursor.execute("SELECT ccode FROM brands")
+	cursor.execute("SELECT ccode FROM brands where ccode = 1821")
 	res = cursor.fetchall()
 	for r in res:
 		try:
 			data = q.get_historical_prices(r[0], jsm.DAILY, start_date = start_date, end_date = end_date)
 			ts = datetime.datetime.today().strftime("%Y-%m-%d %H:%M:%S")
-			[cursor.execute("INSERT INTO prices(ccode, date, open, high, low, close, volume, created_at, updated_at) VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s)", [r[0], d.date, d.open, d.high, d.low, d.close, d.volume, ts, ts]) for d in data]
-			connector.commit()
+			
+			for d in data:
+				try:
+					cursor.execute("INSERT INTO prices(ccode, date, open, high, low, close, volume, created_at, updated_at) VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s)", [r[0], d.date, d.open, d.high, d.low, d.close, d.volume, ts, ts])
+					connector.commit()
+				except:
+					pass
 		except:
-			print "Error in Price Data ", r[0]
+			pass
 
 	cursor.close()
 	connector.close()
